74f1ea1d342088b9bf166c8bca4c7e0d42de4943
bhoria ashish <ashishbhoria12@gmail.com>
2025-08-19 16:20:37 +0530

    Second commit

--- src/app/Store.js
+++ src/app/Store.js
@@ -2,6 +2,8 @@ import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/AuthSlice'
 import ProductReducer from '../features/products/ProductSlice';
 
+
+//Redux Store
 export const store = configureStore({
     reducer:{
         auth:authReducer,
